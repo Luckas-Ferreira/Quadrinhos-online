f78cac8d2ec516b1f6a6ab7af6cbb109159e10db
luckas-ferreira <luckas0ferreira321@gmail.com>
2024-02-14 12:12:31 -0300

    feat: delete quadrinho finalizado com sucesso

--- client/src/app/components/pages/show-quandrinhos/see-quadrinho/see-quadrinho.component.ts
+++ client/src/app/components/pages/show-quandrinhos/see-quadrinho/see-quadrinho.component.ts
@@ -1,8 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, TemplateRef } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Quadrinho } from 'src/app/interfaces/quadrinho';
 import { QuadrinhoService } from 'src/app/services/quadrinho.service';
 import { Base } from 'src/app/environments/baseUrl';
+import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
 @Component({
   selector: 'app-see-quadrinho',
   templateUrl: './see-quadrinho.component.html',
@@ -10,9 +11,15 @@ import { Base } from 'src/app/environments/baseUrl';
 })
 export class SeeQuadrinhoComponent implements OnInit{
   baseUrl = Base.url;
+  modalRef!: BsModalRef;
+  quadrinho_id: number = 0;
+  fraseAlert: string = '';
   dataQuadrinho!: Quadrinho['quadrinho'];
+  config2 = {
+    class: 'modal-dialog-centered'
+  }
 
-  constructor(private router: Router, private route: ActivatedRoute, private quadrinho: QuadrinhoService){}
+  constructor(private router: Router, private route: ActivatedRoute, private quadrinho: QuadrinhoService,  private modalService: BsModalService){}
   ngOnInit(): void {
     
     this.route.params.subscribe(params => {
@@ -22,6 +29,12 @@ export class SeeQuadrinhoComponent implements OnInit{
     });
   }
 
+
+  openModal(quadrinho_id: any, template: TemplateRef<any>){  
+    this.modalRef = this.modalService.show(template, this.config2)
+    this.quadrinho_id = quadrinho_id
+   }
+   
   get Router(){
     return this.router.url;
   }
@@ -32,6 +45,26 @@ export class SeeQuadrinhoComponent implements OnInit{
       }else{
         
       }
+    })
+  }
+
+  deleteQuadrinho(){
+    this.quadrinho.deleteQuadrinho({quadrinho_id: this.quadrinho_id}).subscribe((response: Quadrinho) => {
+        if(response.ok){
+          this.fraseAlert = 'Quadrinho removido com sucesso'!;
+             const alert = document.getElementById('true');
+              alert!.classList.remove('d-none');
+             setTimeout(() => {
+                this.router.navigateByUrl('/admin/quadrinhos')
+               alert!.classList.add('d-none')}, 2000);
+          }else{
+            this.fraseAlert = response.message;
+            const alert = document.getElementById('false');
+            alert!.classList.remove('d-none');
+            setTimeout(() => {
+              alert!.classList.add('d-none')}, 4000);
+          }
+          this.modalRef.hide();
     })
   }
 
